b59135ed2fa9b814232e55c6e6a8be1fdf33024d
Paramtamtam <slashkot@gmail.com>
2019-11-05 21:58:33 +0500

    Options added

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module mikrotik-hosts-parser
 
 go 1.13
 
-require github.com/gorilla/mux v1.7.3
+require (
+	github.com/gorilla/mux v1.7.3
+	github.com/jessevdk/go-flags v1.4.0
+)
--- main.go
+++ main.go
@@ -1,16 +1,33 @@
 package main
 
-import "time"
+import (
+	"log"
+	"os"
+	"time"
+)
 
 const VERSION = "3.0.0" // Do not forget update this value before new version releasing
 
 func main() {
+	var (
+		stdLog = log.New(os.Stderr, "", 0)
+		errLog = log.New(os.Stderr, "", log.LstdFlags)
+	)
+
+	// Precess CLI options
+	options := NewOptions(stdLog, errLog, func(code int) {
+		os.Exit(code)
+	})
+
+	// Parse options and make all checks
+	options.Parse()
+
 	server := NewServer(&HttpServerSettings{
-		Host:             "0.0.0.0",
-		Port:             8080,
-		PublicDir:        "./resources/public",
-		IndexFile:        "index.html",
-		Error404File:     "404.html",
+		Host:             options.Address,
+		Port:             options.Port,
+		PublicDir:        options.ResourcesDir,
+		IndexFile:        options.IndexFileName,
+		Error404File:     options.Error404FileName,
 		WriteTimeout:     time.Second * 15,
 		ReadTimeout:      time.Second * 15,
 		KeepAliveEnabled: false,
